e9413ee046e8a6dd16a2a02d44f0372934026543
Maksim <maksim@Maksims-MacBook-Pro.local>
2024-01-09 16:16:02 +0100

    ADD: validators

--- libs/post/src/domain/index.ts
+++ libs/post/src/domain/index.ts
@@ -1,2 +1,2 @@
 export * from './post.interface';
-export * from './post.aggregate;'
+export * from './post.aggregate';
--- libs/post/src/domain/post.aggregate.ts
+++ libs/post/src/domain/post.aggregate.ts
@@ -1,14 +1,33 @@
 import { AggregateRoot } from "@nestjs/cqrs";
 import { IPost } from "./post.interface";
 import { randomStringGenerator } from '@nestjs/common/utils/random-string-generator.util';
+import { PostServices } from "./services";
+import { IsBoolean, IsNotEmpty, IsString, IsUUID, validateSync } from "class-validator";
+import { Exclude } from "class-transformer";
 
-export class PostAggregate extends AggregateRoot implements IPost{
+export class PostAggregate extends PostServices implements IPost {
+    @IsUUID()
     id: string = randomStringGenerator();
+
+    @IsString()
+    @IsNotEmpty()
     title: string;
+
+    @IsString()
+    @IsNotEmpty()
     message: string;
+
+    @IsUUID()
     authorId: string;
+
+    @IsBoolean()
+    @Exclude()
     published = false;
+
+    @IsString()
     createdAt= new Date().toISOString();
+
+    @IsString()
     upatedAt= new Date().toISOString();
 
     private constructor() {
@@ -19,6 +38,11 @@ export class PostAggregate extends AggregateRoot implements IPost{
         const _post = new PostAggregate();
 
         Object.assign(_post, post);
+        _post.upatedAt = post?.id ? new Date().toISOString() : _post.upatedAt;
+
+        const errors = validateSync(_post, {whitelist: true});
+
+        if (!!errors.length) throw new Error("Post not valid. ");
 
         return _post;
     }
