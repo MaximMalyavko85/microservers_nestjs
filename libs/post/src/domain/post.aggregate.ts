ff4b74d8e5a726b94b8a011bbd6f6c8505c5ce7c
Maksim <maksim@Maksims-MacBook-Pro.local>
2024-01-09 16:28:15 +0100

    ADD: errors & domain errors

--- libs/post/src/domain/post.aggregate.ts
+++ libs/post/src/domain/post.aggregate.ts
@@ -4,6 +4,7 @@ import { randomStringGenerator } from '@nestjs/common/utils/random-string-genera
 import { PostServices } from "./services";
 import { IsBoolean, IsNotEmpty, IsString, IsUUID, validateSync } from "class-validator";
 import { Exclude } from "class-transformer";
+import { DomainError } from "@lib/errors";
 
 export class PostAggregate extends PostServices implements IPost {
     @IsUUID()
@@ -42,7 +43,7 @@ export class PostAggregate extends PostServices implements IPost {
 
         const errors = validateSync(_post, {whitelist: true});
 
-        if (!!errors.length) throw new Error("Post not valid. ");
+        if (!!errors.length) throw new DomainError(errors, 'Post not valid.');
 
         return _post;
     }
